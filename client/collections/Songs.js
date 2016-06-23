80ba0b7f2bed28bba927b25d15515c2633cb5244
JasonArkens17 <jason.arkens17@gmail.com>
2016-06-23 15:17:44 -0700

    Finished Ajax request portion. Got all songs to load from server.

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,23 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
+  url: 'api.parse.com/1/classes/songs/',
+
+  parse: function(data) {
+    return data; 
+  },
+
+  fetch: function(songs) {
+    songs = songs || {}; 
+    return Backbone.Collection.prototype.fetch.call(this, songs); 
+  },
+
 
   model: SongModel
+
+  // initialize: function() {
+  //   this.fetch();
+  // }
+
+
 
 });
