13c3382552d77b11f0a735765b7d9144000440e6
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 15:34:05 -0700

    fix syntax issues and move to new repo

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,24 +1,18 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
-  url: 'https://api.parse.com/1/classes/songs/', 
 
+  url: 'https://api.parse.com/1/classes/songs/',
 
-  parse: function(data) {
-    return data.results;
-  }, 
+  parse: function(response) {
+    return response.results;
+  },
 
   initialize: function() {
-    this.fetch(); 
-  }, 
-
-
+    this.fetch({
+      data: { limit: 10 }
+    });
+  },
 
   model: SongModel
-
-  // initialize: function() {
-  //   this.fetch();
-  // }
-
-
 
 });
