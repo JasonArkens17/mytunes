68ef40ba7db7b0488359d8b0e625dbe937b45de1
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 15:36:56 -0700

    clean up comments

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,24 +1,23 @@
-  // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
-  
+
   play: function() {
-    // Triggering an event here will also trigger the event on the collection
+    
     this.trigger('play', this);
   },
 
   enqueue: function() {
     this.trigger('enqueue', this);
-  }, 
+  },
 
   dequeue: function() {
     this.trigger('dequeue', this);
-  }, 
-
-  ended: function() {
-    this.trigger('ended', this); 
   },
 
-  
+  ended: function() {
+    this.trigger('ended', this);
+  },
+
+
   initialize: function() {
     this.set('playCount', 0);
   }
