11e68396371a48dbb1f5a26e949934ee99290414
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 15:38:27 -0700

    clean up AppView

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -5,20 +5,16 @@ var AppView = Backbone.View.extend({
     console.log(params);
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
-    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
-    // change:currentSong - this is Backbone's way of allowing you to filter events to
-    // ONLY receive change events for the specific property, 'currentSong'
-    this.model.on('change:currentSong', function(model) {
-      this.playerView.setSong(model.get('currentSong'));
-    }, this);
 
-    //this.odel.on(changesongque)
-    //  re-redner
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
+
+    this.model.on('change:currentSong', function(model) {
+        this.playerView.setSong(model.get('currentSong'));
+      }, this);
 
     this.model.on('change:songQueue', function(model) {
-      console.log('did this fire?');
     });
-      //this.info = songqueue.append new(song)
+    
   },
 
   render: function() {
